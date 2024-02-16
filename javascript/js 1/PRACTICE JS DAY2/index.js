ce99e4cb61afe72753125ae3b485622b4a96a02c
saurav82923 <saurav14032000@gmail.com>
2024-02-16 13:55:42 +0530

    practice
    practice

---
+++ javascript/js 1/PRACTICE JS DAY2/index.js
@@ -0,0 +1,14 @@
+console.log('saurav is wo');
+let lastName= 'kumar';
+let arraywords=' hi i \n "a"m \n saurav';
+console.log(arraywords);
+let abc= `this is 
+new 
+line`;
+console.log(abc);
+let date= new Date();
+console.log(date);
+let farray=[1,2,3];
+let sarray=[4,5,6];
+let combinedarray= farray.concat(sarray);
+console.log(combinedarray);
